3cf3e04e819e98ef2d438a7071d00ad660eee0ad
Cefalu Wave <luke.evan.graham@gmail.com>
2021-07-16 21:17:18 -0700

    working on project reducer

--- client/src/store/reducers/project.js
+++ client/src/store/reducers/project.js
@@ -5,6 +5,26 @@ const initialState = {
   currentProject: {
     issues: [],
   },
+  issues: {},
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "To Do",
+      issueIds: [],
+    },
+    "column-2": {
+      id: "column-2",
+      title: "Doing",
+      issueIds: [],
+    },
+    "column-3": {
+      id: "column-3",
+      title: "Done",
+      issueIds: [],
+    },
+  },
+  // FACILITATE REORDERING OF THE COLUMNS
+  columnOrder: ["column-1", "column-2", "column-3"],
 };
 
 const projectReducer = (state = initialState, action) => {
@@ -17,7 +37,18 @@ const projectReducer = (state = initialState, action) => {
     case actionTypes.GET_PROJECTS:
       return { ...state, projects: action.payload };
     case actionTypes.GET_PROJECT:
-      return { ...state, currentProject: action.payload };
+      const objectOfIssues = action.payload.issues.reduce(
+        (result, item, index, array) => {
+          result[item._id] = item;
+          return result;
+        }
+      );
+      console.log("HERE: ", objectOfIssues);
+      return {
+        ...state,
+        currentProject: action.payload,
+        issues: objectOfIssues,
+      };
     case actionTypes.ADD_ISSUE:
       return {
         ...state,
