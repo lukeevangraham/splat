ddc090630bfe8979792d1a6146afa89afe74b3f8
Cefalu Wave <luke.evan.graham@gmail.com>
2021-07-31 23:03:51 -0700

    basic styles for the columns

--- client/src/components/Column/Column.js
+++ client/src/components/Column/Column.js
@@ -23,14 +23,14 @@ const Column = ({ column, issues, projectId, addIssue }) => {
   };
 
   return (
-    <div className="m-2 border-2">
-      <h3 className="p-2">{column.title}</h3>
+    <div className="m-2 border-2 rounded bg-gradient-to-br from-gray-100 to-gray-200">
+      <div className="pt-2 px-3 pb-1 text-xl font-medium">{column.title}</div>
       <Droppable droppableId={column.id}>
         {(provided) => (
           <div
             ref={provided.innerRef}
             {...provided.droppableProps}
-            className="p-2"
+            className="px-2"
           >
             {issues.map((issue, index) => (
               <Issue key={issue._id} issue={issue} index={index} />
--- client/src/components/Column/Issue/Issue.js
+++ client/src/components/Column/Issue/Issue.js
@@ -8,6 +8,7 @@ const Issue = ({ issue, index }) => (
         {...provided.draggableProps}
         {...provided.dragHandleProps}
         ref={provided.innerRef}
+        className="border-solid border-2 p-2 my-1.5 rounded bg-white"
       >
         {issue.summary}
       </div>
