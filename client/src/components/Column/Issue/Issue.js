7f952d0bc4f6a40965a57a5d2c3195710fc9fa26
Cefalu Wave <luke.evan.graham@gmail.com>
2021-08-09 00:06:37 -0700

    pencil icon

--- client/src/components/Column/Issue/Issue.js
+++ client/src/components/Column/Issue/Issue.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Draggable } from "react-beautiful-dnd";
-import { TrashIcon } from "@heroicons/react/outline";
+import { PencilAltIcon } from "@heroicons/react/outline";
 
 const Issue = ({ issue, index, editing }) => (
   <Draggable draggableId={issue._id} index={index}>
@@ -12,7 +12,7 @@ const Issue = ({ issue, index, editing }) => (
         className="border-solid border-1 p-1.5 my-1.5 rounded bg-white flex"
       >
         <div className="flex-grow">{issue.summary}</div>
-        <TrashIcon className="h-5 w-5" onClick={() => editing(issue)} />
+        <PencilAltIcon className="h-5 w-5" onClick={() => editing(issue)} />
       </div>
     )}
   </Draggable>
