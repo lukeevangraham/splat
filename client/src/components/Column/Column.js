3f0f4df00fc773f91f085e4d45e4c362da3da521
Cefalu Wave <luke.evan.graham@gmail.com>
2021-08-11 23:38:15 -0700

    new styles of add Issue

--- client/src/components/Column/Column.js
+++ client/src/components/Column/Column.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Droppable } from "react-beautiful-dnd";
 import Issue from "./Issue/Issue";
+import { XIcon } from "@heroicons/react/outline";
 import { addIssue } from "../../store/actions/project";
 
 const Column = ({ column, issues, projectId, addIssue, editing }) => {
@@ -42,17 +43,19 @@ const Column = ({ column, issues, projectId, addIssue, editing }) => {
                   type="text"
                   name="newIssueSummary"
                   id="newIssueSummary"
+                  placeholder="Enter a summary for this issue"
+                  autoFocus
                   value={newIssueSummary}
                   onChange={(event) => inputChangedHandler(event)}
                   className="p-1.5 w-full"
                 />
-                <div>
-                  <button className="text-gray-600">Add issue</button>
-                  <button className="text-gray-600 ml-3">X</button>
+                <div className="flex items-center mt-1">
+                  <button className="bg-blue-500 py-1 px-4 rounded text-gray-100">Add issue</button>
+                  <button className="text-gray-600 ml-3 hover:text-gray-900 py-1"><XIcon className="h-5 w-5" /></button>
                 </div>
               </form>
             ) : (
-              <div className="my-3 mx-1.5 text-gray-600" onClick={() => setInputtingNewIssue(true)}>
+              <div className="my-3 mx-1.5 text-gray-600 cursor-pointer" onClick={() => setInputtingNewIssue(true)}>
                 + Add an issue
               </div>
             )}
