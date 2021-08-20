378f3c82c7117c7e3e16943b327107aafab63f47
Cefalu Wave <luke.evan.graham@gmail.com>
2021-08-19 23:04:17 -0700

    modal has media query

--- client/src/components/UI/Modal/Modal.js
+++ client/src/components/UI/Modal/Modal.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Backdrop from "../Backdrop/Backdrop";
+import classes from "./Modal.module.css";
 
 const Modal = ({ show, modalClosed, children }) => (
   <>
     <Backdrop show={show} clicked={modalClosed} />
     <div
-      className="fixed z-50 w-2/3 shadow p-3 left-16/100 top-3/10 bg-white rounded-lg flex flex-col transition-all duration-300"
+      className={`${classes.modal} fixed z-50 w-2/3 sm:w-[500px] shadow p-3 left-16/100 top-3/10 bg-white rounded-lg flex flex-col transition-all duration-300"`}
       style={{
         transform: show ? `translateY(0)` : `translateY(-100vh)`,
         opacity: show ? `1` : `0`,
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     inset: {
       "16/100": "16.5%",
-      "3/10": "30%"
+      "3/10": "30%",
     },
     // width: {
     //   "7/10": "70%"
